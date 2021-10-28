dfc6a1f4781498c90ae64058857cc81b953e48f8
devkiloton <dev.kiloton@gmail.com>
2021-10-28 15:33:20 -0300

    creating chat screen

--- firebase.js
+++ firebase.js
@@ -3,12 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBAC16JnVlmGdazWPj2kBNVrwIwTG0oaf4",
-  authDomain: "private-3dcb9.firebaseapp.com",
-  projectId: "private-3dcb9",
-  storageBucket: "private-3dcb9.appspot.com",
-  messagingSenderId: "871767044700",
-  appId: "1:871767044700:web:922f4462a1901f51286c99"
+  xxxxxxxxxxxxxxxxxxxxxxxxx
 };
 
 let app;
